122f9c0c0c180656612ff6b6dc0b0cdc72baa5c8
AidanNelson <AidanNelson@users.noreply.github.com>
2024-09-03 17:18:38 -0400

    use pageXY

--- examples/mouseScript.js
+++ examples/mouseScript.js
@@ -1,11 +1,12 @@
 console.log('hello live web!')
 let icons = ['🌐', '🎉', '💻', '👾', '🐁']
 function addIcon(ev) {
+    console.log(ev)
     let newElement = document.createElement('p')
     newElement.innerHTML = icons[Math.floor(Math.random() * icons.length)]
     newElement.style.position = 'absolute'
-    newElement.style.left = ev.clientX + 'px'
-    newElement.style.top = ev.clientY + 'px'
+    newElement.style.left = ev.pageX + 'px'
+    newElement.style.top = ev.pageY + 'px'
     newElement.style.transform = 'translate(-50%,-50%)'
     newElement.style.pointerEvents = 'none'
     document.body.appendChild(newElement)
